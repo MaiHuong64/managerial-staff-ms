2cbb26f09daadf959e3900f4a8d081c1f34e07fe
MaiHuong64 <rain06042004@gmail.com>
2026-03-10 07:28:13 +0700

    feat: enhance authentication flow with JWT, update user registration and login logic, and secure routes

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import authController from "../controllers/authController";
+import { verifyToken as verify } from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.post('/register', authController.registerUser);
-router.post('/login', authController.loginUser);
-router.post('/logout', authController.logoutUser);
+router.post('/register', verify ,authController.registerUser);
+router.post('/login', verify, authController.loginUser);
+router.post('/logout', verify, authController.logoutUser);
 
 export default router;
