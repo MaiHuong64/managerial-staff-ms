e2ce888999ce7daf469e2765415dd7a3b8be1ad3
MaiHuong64 <rain06042004@gmail.com>
2026-03-12 19:11:02 +0700

    feat: remove unused types and profile routes, and implement staff creation and profile retrieval logic

--- server/routes/staff.ts
+++ server/routes/staff.ts
@@ -3,10 +3,10 @@ import staffController from "../controllers/staffController";
 import { verifyToken } from "../middleware/authMiddleware";
 const router = Router();
 
+router.get('/profile', verifyToken, staffController.getProfileStaff);
 router.get('/', verifyToken, staffController.getAllStaff);
 router.get('/:id', verifyToken, staffController.getStaffById);
 router.post('/', verifyToken, staffController.createStaff);
 router.put('/:id', verifyToken, staffController.updateStaff);
 router.delete('/:id', verifyToken, staffController.deleteStaff);
-
 export default router;
