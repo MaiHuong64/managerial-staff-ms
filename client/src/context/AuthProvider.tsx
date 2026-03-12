b33d6062e68ef5a24cc0f4eea1d6da56da53f925
MaiHuong64 <rain06042004@gmail.com>
2026-03-12 07:23:22 +0700

    feat: implement user authentication and profile retrieval, update routing and context management

--- client/src/context/AuthProvider.tsx
+++ client/src/context/AuthProvider.tsx
@@ -1,10 +1,14 @@
-import React, { useState,  } from "react";
-import type { AuthUser } from "../types/auth";
+import React, {useState,  } from "react";
+import type { AuthUser } from "../types/AuthUser";
 import type { ReactNode } from "react";
 import { AuthContext } from "./AuthContext";
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [user, setUser] = useState<AuthUser | null>(null);
+    const [user, setUser] = useState<AuthUser | null>(() =>
+    {
+        const saveUser = localStorage.getItem("user");
+        return saveUser? JSON.parse(saveUser): null;
+    });
 
     //Lưu trữ token 
     const [token, setToken] = useState<string | null>(
@@ -14,19 +18,19 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     const login = (userData: AuthUser, token: string) =>{
         setUser(userData);
         setToken(token);
-        //restore token when page reload
         localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(userData));
     };
-
     const logout = () =>{
         setUser(null);
         setToken(null);
         localStorage.removeItem("token");
+        localStorage.removeItem("user")
         // window.location.href = '/login';
     }
     return(
-       <AuthContext.Provider value={{ user, token, login, logout }}>
-            {children} 
+        <AuthContext.Provider value={{ user, token, login, logout, isAuthenticated: !!token }}>
+            {children}
         </AuthContext.Provider>
     );
 };
--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -1,23 +1,4 @@
 export default interface LoginType {
   ten_dang_nhap: string;
   mat_khau: string;
-  vai_tro: string
-}
-export type VaiTro = 'VC' | 'VCQL' | 'BGH' | 'PTCCT';
-
-export interface AuthUser{
-  id: number;
-  ten_dang_nhap: string;
-  vai_tro: VaiTro;
-  vien_chuc_id: number;
-  ho_va_ten: string;
-  avatar: string | null;     
-  don_vi_id: number;
-  ten_don_vi: string;
-}
-export interface AuthContextType{
-  user: AuthUser | null;
-  token: string | null;
-  login: (user: AuthUser, token: string) => void;
-  logout: () => void;
 }
