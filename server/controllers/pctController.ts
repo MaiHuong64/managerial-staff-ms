3235e1c330133281cf9ea98a88a469ed470c9165
MaiHuong64 <rain06042004@gmail.com>
2026-03-13 22:10:42 +0700

    feat: implement approve PTC functionality and adjust routes

--- server/controllers/pctController.ts
+++ server/controllers/pctController.ts
@@ -40,7 +40,7 @@ const pctController = {
                             so_luong_de_xuat, nguon_nhan_su, dot_quy_hoach_id,
                             chuc_danh_id, don_vi_id, nguoi_lap_id, trang_thai) 
                             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`;
-            const result = await pool.query(query, [so_to_trinh_chu_truong, tieu_de, ly_do_de_xuat, so_luong_de_xuat,nguon_nhan_su, dot_quy_hoach_id, chuc_danh_id, don_vi_id, uid,  1]);
+            const result = await pool.query(query, [so_to_trinh_chu_truong, tieu_de, ly_do_de_xuat, so_luong_de_xuat,nguon_nhan_su, dot_quy_hoach_id, chuc_danh_id, don_vi_id, uid, 0]);
             
             if (result.rows[0])
                 return res.status(201).json(result.rows[0]);
@@ -53,9 +53,30 @@ const pctController = {
     },
     approvePTC: async (req: Request, res: Response) => {
         try {
-            
+            const {uid, vai_tro} = (req as any).user;
+            const {id} = req.params;
+            const {trang_thai, ly_do} = req.body;
+            const pct = await pool.query(`select * from phieu_chu_truong where id = $1`, [id]);
+
+            // console.log("ket qua: ", pct);
+            if (pct.rows.length === 0)  return res.status(404).json({ message: 'Không tìm thấy phiếu' });
+
+            if (vai_tro === "BGH"){
+                const query = `update phieu_chu_truong
+                                set trang_thai = $1,
+                                    ly_do_de_xuat = $2,
+                                    ngay_phe_duyet = $3
+                                    where id = $4 RETURNING *`
+                const result = await pool.query(query, [trang_thai, ly_do, new Date(), id]);
+
+                console.log("rows:", result.rows); 
+                console.log("rowCount:", result.rowCount);
+
+                return res.status(200).json(result.rows[0]);
+            }
         } catch (error) {
-            
+            console.error(error);
+            return res.status(500).json({ success: false, message: "Lỗi server" });
         }
     }
 }
