f73c92c3dd2d425a84942d434dee2b2a498af3ae
MaiHuong64 <rain06042004@gmail.com>
2026-03-25 15:31:39 +0700

    refactor: update PersonnelProposalPage to use PhuongAnNhanSu type and improve data handling

--- client/src/types/PhuongAnNhanSu.ts
+++ client/src/types/PhuongAnNhanSu.ts
@@ -1,3 +1,9 @@
-export interface PhuongAnNhanSu{
-  
+export interface PhuongAnNhanSu {
+    id: number;
+    ma_phuong_an: string;
+    so_to_trinh: string;
+    ngay_to_trinh: string;
+    ngay_lap: string;
+    trang_thai: number;
+    so_nhan_su?: number ;
 }
