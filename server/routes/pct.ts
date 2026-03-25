2009dfb8fee1719bd1d10254acb9481d8cb4ff63
MaiHuong64 <rain06042004@gmail.com>
2026-03-25 22:54:29 +0700

    feat: Refactor CreatePhieuChuTruongModal and PhieuChuTruongPage for improved data handling and UI updates

--- server/routes/pct.ts
+++ server/routes/pct.ts
@@ -5,6 +5,7 @@ import { checkRole, verifyToken } from "../middleware/authMiddleware";
 const router = Router();
 
 router.get('/', verifyToken,checkRole(["VCQL", "PTCCT", "BGH"]) ,pctController.getAllPTC)
+router.get('/dot-quy-hoach', verifyToken, checkRole(["VCQL", "PTCCT"]), pctController.getDotQuyHoach)
 router.post('/', verifyToken, checkRole(["VCQL"]), pctController.createPTC)
 router.put('/:id', verifyToken, checkRole(["BGH"]), pctController.approvePTC)
 export default router;
