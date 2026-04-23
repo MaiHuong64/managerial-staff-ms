cdbf3c5ac6e95fcf1dacf2438cc5101684176504
MaiHuong64 <rain06042004@gmail.com>
2026-04-23 18:09:27 +0700

    feat: update VCQLDashboard to fetch and display data dynamically; remove ApprovePhuongAnPage; refactor type definitions for consistency

--- client/src/types/NhiemKyChucVu.ts
+++ client/src/types/NhiemKyChucVu.ts
@@ -1,7 +1,7 @@
 export interface NhiemKyChucVu {
     id: number;
-    ngay_bat_dau: string;
-    ngay_ket_thuc: string;
-    ly_do_ket_thuc: string;
-    trang_thai: number;
+    ngayBatDau: string;
+    ngayKetThuc: string;
+    lyDoKetThuc: string;
+    trangThai: number;
 }
