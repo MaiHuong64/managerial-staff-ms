3ddb5603f65814729912718fe43b1e39b1c54d15
MaiHuong64 <rain06042004@gmail.com>
2026-05-02 19:03:58 +0700

    create a don vi page

--- client/src/api/donVi.api.ts
+++ client/src/api/donVi.api.ts
@@ -1,3 +1,4 @@
+import type { CreateDonVi, DonVi } from "../types/DonVi";
 import axiosClient from "../utils/AxiosClient";
 
 export const getDonViList = () =>
@@ -5,4 +6,9 @@ export const getDonViList = () =>
 
 export const getDonViById = (id: number) =>
     axiosClient.get(`/don-vi/${id}`);
- 
+ export const createDonVi = (payload: CreateDonVi) =>
+    axiosClient.post("/don-vi", payload);
+export const updateDonVi = (id: number, payload: DonVi) =>
+    axiosClient.put(`/don-vi/${id}`, payload);
+export const deleteDonVi = (id: number) =>
+    axiosClient.delete(`/don-vi/${id}`);
--- client/src/types/DonVi.ts
+++ client/src/types/DonVi.ts
@@ -1,5 +1,21 @@
 export interface DonVi {
     id: number;
+    maDonVi: string;
     tenDonVi: string;
     loaiDonVi: string;
+    donViCha?: number;
+    diaChi?: string;
+    soDienThoai?: string;
+    email?: string;
+    trangThai: number;
+    ngayThanhLap?: string;
+    tenTruongDonVi?: string;
+    tenPhoDonVi?: string;
+}
+export interface CreateDonVi {
+    tenDonVi: string;
+    donViCha?: number;
+    soDienThoai?: string;
+    email?: string;
+    diaChi?: string;
 }
